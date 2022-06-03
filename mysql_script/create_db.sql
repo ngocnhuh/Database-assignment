43c945956bbab27bfaa066590b5d08d1553ac947
ngocnhuh <nhu.huynh1142002@hcmut.edu.vn>
2022-06-03 21:51:18 +0700

    fix loi chinh ta discription

--- mysql_script/create_db.sql
+++ mysql_script/create_db.sql
@@ -172,7 +172,7 @@ CREATE TABLE `Passenger_ticket`(
 CREATE TABLE `Luggage_ticket`(
   `ticket_id` int(11) ZEROFILL NOT NULL,
   `weight` int NOT NULL,
-  `discription` varchar(100) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`ticket_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
