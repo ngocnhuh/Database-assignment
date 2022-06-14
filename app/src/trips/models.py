d4857023cbf9b8d835ef487af8a4731d1f338610
unknown <hung406474@gmail.com>
2022-06-14 17:18:28 +0700

    fix bug add trip

--- app/src/trips/models.py
+++ app/src/trips/models.py
@@ -147,22 +147,23 @@ class Trip(models.Model):
                 raise ValidationError({'driver':'Driver not available'})
         
         # bus_staff
-        trip_staffs = self.trip_staffs.all()
-        overlaped_staffs = []
-        for tf in trip_staffs:
-            tf_sched = tf.trips.all()
-            if self.trip_id is not None:
-                tf_sched=tf_sched.exclude(trip_id = self.trip_id)
-            for trip in tf_sched:
-                departure_dt = datetime.combine(trip.departure_date, trip.sched.departure_time)
-                arrival_dt = datetime.combine(trip.departure_date, trip.sched.arrival_time)
-                if not (departure_sched-arrival_dt > MIN_DELTA_TIME_TRIP_SPAN \
-                    or departure_dt-arrival_sched > MIN_DELTA_TIME_TRIP_SPAN):
-                    overlaped_staffs.append(f'{tf.fname} {tf.lname}')
-                    break
-        if len(overlaped_staffs) != 0:
-            error = f'{overlaped_staffs} not available'
-            raise ValidationError({'trip_staffs':error})
+        if self.trip_id:
+            trip_staffs = self.trip_staffs.all()
+            overlaped_staffs = []
+            for tf in trip_staffs:
+                tf_sched = tf.trips.all()
+                if self.trip_id is not None:
+                    tf_sched=tf_sched.exclude(trip_id = self.trip_id)
+                for trip in tf_sched:
+                    departure_dt = datetime.combine(trip.departure_date, trip.sched.departure_time)
+                    arrival_dt = datetime.combine(trip.departure_date, trip.sched.arrival_time)
+                    if not (departure_sched-arrival_dt > MIN_DELTA_TIME_TRIP_SPAN \
+                        or departure_dt-arrival_sched > MIN_DELTA_TIME_TRIP_SPAN):
+                        overlaped_staffs.append(f'{tf.fname} {tf.lname}')
+                        break
+            if len(overlaped_staffs) != 0:
+                error = f'{overlaped_staffs} not available'
+                raise ValidationError({'trip_staffs':error})
 
 
 
@@ -170,7 +171,10 @@ class Trip(models.Model):
         self.full_clean()
 
         self.update_empty_seats()
+        if not self.trip_id:
+            super(Trip, self).save(*args, **kwargs)
         super(Trip, self).save(*args, **kwargs)
+        
 
     def update_empty_seats(self):
         tickets = self.tickets.all()
